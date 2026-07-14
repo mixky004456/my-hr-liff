2345e00a26f9152c213b702a7c74b00029b29aba
mixky004456 <tomine3510@gmail.com>
2026-07-14 14:45:02 +0700

    api

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // config.js
 // ใส่ URL ของ Google Apps Script ที่ Deploy เป็น Web App (แบบ JSON API) ตรงนี้
-const GAS_API_URL = "https://script.google.com/macros/s/AKfycbyLPWmM-LDWnMOhEGUycivgh24BVEPN5G2kbbWgAHIifg9m0tBgavflmHByecg2Vwia/exec"; // กรุณาเปลี่ยนเป็น URL ใหม่ที่ได้หลังจาก Deploy api.js
+const GAS_API_URL = "https://script.google.com/macros/s/AKfycbxs8pXq08BAap85XoVA7fB3lZOeQ_4jTOoHuE_Csvhk23cdaXe-Q9U6p26tSC-rzRwR0Q/exec"; // กรุณาเปลี่ยนเป็น URL ใหม่ที่ได้หลังจาก Deploy api.js
