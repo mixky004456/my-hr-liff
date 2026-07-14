206f4a0dcbb17296e7b71832840836b90f442528
mixky004456 <tomine3510@gmail.com>
2026-07-14 14:52:28 +0700

    แก้ชีตไอดี

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // config.js
 // ใส่ URL ของ Google Apps Script ที่ Deploy เป็น Web App (แบบ JSON API) ตรงนี้
-const GAS_API_URL = "https://script.google.com/macros/s/AKfycbxs8pXq08BAap85XoVA7fB3lZOeQ_4jTOoHuE_Csvhk23cdaXe-Q9U6p26tSC-rzRwR0Q/exec"; // กรุณาเปลี่ยนเป็น URL ใหม่ที่ได้หลังจาก Deploy api.js
+const GAS_API_URL = "https://script.google.com/macros/s/AKfycbyDGpk1KHIvSsZKN_1akbUpyCvpOTwA8cdnxMo3wrp2dgC1Pa7iD1nh9xetOaWXKTshRQ/exec"; // กรุณาเปลี่ยนเป็น URL ใหม่ที่ได้หลังจาก Deploy api.js
