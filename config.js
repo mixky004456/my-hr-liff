077db12497ca77f7e089b4602293992a2d336269
mixky004456 <tomine3510@gmail.com>
2026-07-23 10:54:17 +0700

    แก้นิดหน่อย

--- config.js
+++ config.js
@@ -1,3 +1,1 @@
-// config.js
-// ใส่ URL ของ Google Apps Script ที่ Deploy เป็น Web App (แบบ JSON API) ตรงนี้
-const GAS_API_URL = "https://script.google.com/macros/s/AKfycbyDGpk1KHIvSsZKN_1akbUpyCvpOTwA8cdnxMo3wrp2dgC1Pa7iD1nh9xetOaWXKTshRQ/exec"; // กรุณาเปลี่ยนเป็น URL ใหม่ที่ได้หลังจาก Deploy api.js
+const GAS_API_URL = "https://script.google.com/macros/s/AKfycbyDGpk1KHIvSsZKN_1akbUpyCvpOTwA8cdnxMo3wrp2dgC1Pa7iD1nh9xetOaWXKTshRQ/exec"; 
